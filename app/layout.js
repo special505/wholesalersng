06b08c72f33cfddbeb6e2bf14ac45555b9789437
Godfrey James <godfreyjames1999@gmail.com>
2024-11-08 04:36:20 -0800

    authentication

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
-import{ Navbar} from "@/components/Navbar"
+import{ Navbar} from "@/components/Navbar";
+import { SessionProvider} from "next-auth/react"
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,9 +26,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+   
+   <SessionProvider>
         <Navbar/>
         {children}
         <Footer/>
+   </SessionProvider>
       </body>
     </html>
   )
